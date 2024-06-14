8bfd0c10713d38047704669ad67dec17a853c20a
SaptarshiAcharya <saptarshiironman4512@gmail.com>
2024-06-14 22:48:38 +0530

    worked on the events page

--- src/app/event_page/page.js
+++ src/app/event_page/page.js
@@ -1,5 +1,8 @@
 'use client';
 import './styles/App.css';
+import Image from 'next/image';
+import im1 from './ev1.jpg';
+import im2 from './ev2.jpg';
 
 export default function Page() {
   const scrollToSection = (sectionId) => {
@@ -19,45 +22,42 @@ export default function Page() {
 
   return (
     <div className="App">
-     
-
       <div className="container">
         <div className="event-box" id="completed-events">
           <h2>Completed Events</h2>
           <div className="event-list">
-            <p><a href="#completed-event-details" onClick={() => scrollToSection('completed-event-details')}>Event A</a></p>
-            <p><a href="#completed-event-details" onClick={() => scrollToSection('completed-event-details')}>Event B</a></p>
+            <p><a href="#completed-event-details" onClick={() => scrollToSection('completed-event-details')}>INNO BLITZ</a></p>
+            <p><a href="#completed-event-details" onClick={() => scrollToSection('completed-event-details')}>DOCKER WORKSHOP</a></p>
           </div>
         </div>
 
         <div className="event-box" id="upcoming-events">
           <h2>Upcoming Events</h2>
           <div className="event-list">
-            <p><a href="#upcoming-event-details" onClick={() => scrollToSection('upcoming-event-details')}>Event C</a> <button className="register-btn">Register</button></p>
-            <p><a href="#upcoming-event-details" onClick={() => scrollToSection('upcoming-event-details')}>Event D</a> <button className="register-btn">Register</button></p>
+            {/*<p><a href="#upcoming-event-details" onClick={() => scrollToSection('upcoming-event-details')}>Event C</a> <button className="register-btn">Register</button></p>
+            <p><a href="#upcoming-event-details" onClick={() => scrollToSection('upcoming-event-details')}>Event D</a> <button className="register-btn">Register</button></p>*/}
+            <p><a href="#upcoming-event-details" onClick={() => scrollToSection('upcoming-event-details')}>EVENTS COMING SOON</a></p>
           </div>
         </div>
       </div>
-
-     
 
       <div className="section-box" id="completed-event-details">
         <section className="section">
           <h2>Completed Event Details</h2>
           <div className="event-container">
             <div className="event-detail" id="event-a-details">
-              <h3>Event A</h3>
-              <p>Details about Event A...</p>
+              <h3>INNO BLITZ</h3>
+              <Image src={im1} width={500} height={500} alt="Inno Blitz Poster" />
             </div>
             <div className="event-detail" id="event-b-details">
-              <h3>Event B</h3>
-              <p>Details about Event B...</p>
+              <h3>DOCKER WORKSHOP</h3>
+              <Image src={im2} width={500} height={500} alt="Docker Workshop Poster" />
             </div>
           </div>
         </section>
       </div>
 
-      <div className="section-box" id="upcoming-event-details">
+      { /* <div className="section-box" id="upcoming-event-details">
         <section className="section">
           <h2>Upcoming Event Details</h2>
           <div className="event-container">
@@ -71,9 +71,7 @@ export default function Page() {
             </div>
           </div>
         </section>
-      </div>
-
-   
+      </div> */ }
 
       <button id="scrollTopBtn" title="Go to top" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>↑</button>
     </div>
